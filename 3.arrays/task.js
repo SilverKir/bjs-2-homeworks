fc8f519696ca6812b67847c9e9308f5e075b9073
silver_kir <rci@mp03.ru>
2024-04-12 20:05:52 +0800

    home3 done

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,10 @@
 function compareArrays(arr1, arr2) {
-  
+    return arr1.length===arr2.length && arr1.every((element, index)=>element===arr2[index]);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
+    return users
+                .filter(user=>user.gender===gender)
+                .reduce((acc, user, indx,arr)=>{return acc+user.age/arr.length},0)
   
 }
